bf4fc54d83bcac5680790f38efbfca4a60f13a86
StepanHo <khomullo.sn@students.dvfu.ru>
2021-04-30 16:51:14 +1000

    Textures and fixed some bugs

--- Script/script.js
+++ Script/script.js
@@ -4,54 +4,44 @@ const ctx = canvas.getContext('2d')
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const player = {
-    x: canvas.width / 2,
-    y: canvas.height - 50,
-    width: 50,
-    height: 50,
-    color: 'lime',
-    dx: 10,
-    dy: 5,
-    health: 3,
+let rightPressed = false
+let leftPressed = false
+let upPressed = false
+let downPressed = false
+let spacePressed = false;
+let score = 0;
+let scoreRecord = 0;
+let isKeyboard = false
+let isMouse = false
+let isTouch = false
+let isMenu = true
+let isGame = false
+let isEnd = false
+let isFirstWave = false
+let isSecondWave = false
+let isThirdWave = false
+let periodOfScore = 5
+let nowTimeOfScore = 0
+const friendBullets = []
+const enemyBullets = []
+const enemies = []
 
-    Draw() {
-        ctx.beginPath()
-        ctx.rect(this.x, this.y, this.width, this.height)
-        ctx.fillStyle = this.color
-        ctx.fill()
-        ctx.closePath()
-    },
-
-    DrawCheck() {
-        if (this.x + 50 > canvas.width) {
-            this.x -= this.dx
-        } else if (this.x < 0) {
-            this.x += this.dx
-        }
-
-        if (this.y + 50 > canvas.height) {
-            this.y -= this.dy
-        } else if (this.y < 0) {
-            this.y += this.dy
-        }
-    },
-
-    Move() {
-        if (rightPressed && !leftPressed) {
-            this.x += this.dx
-        } else if (!rightPressed && leftPressed) {
-            this.x -= this.dx
-        }
-
-        if (upPressed && !downPressed) {
-            this.y -= this.dy
-        } else if (!upPressed && downPressed)
-        {
-            this.y += this.dy
-        }
-
+class Player {
+    constructor(x, y, width = 100, height = 100, dx = 10, dy = 5, health = 3, isGodMod = false, periodOfGodTime = 180, GodModTime = 180) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.dx = dx
+        this.dy = dy
+        this.health = health
+        this.isGodMod = isGodMod
+        this.periodOfGodTime = periodOfGodTime
+        this.GodModTime = GodModTime
     }
 }
+
+let player = new Player(canvas.width / 2 - 25, canvas.height - 100)
 
 class Bullet {
     constructor(x, y, ballRadius, velocity, color) {
@@ -61,28 +51,15 @@ class Bullet {
         this.velocity = velocity
         this.color = color
     }
-
-    Draw() {
-        ctx.beginPath()
-        ctx.arc(this.x, this.y, this.ballRadius, 0, Math.PI * 2, false)
-        ctx.fillStyle = this.color
-        ctx.fill()
-        ctx.closePath()
-    }
-
-    Update() {
-        this.Draw()
-        this.y -= this.velocity
-    }
 }
 
 class Enemy {
-    constructor(x, y, dx, width = 50, height = 50, color = 'red', dy = 1, health = 12, bulletRadius = 5, bulletSpeed = -5, periodOfShooting = 45, timeAtNow = 45) {
+    constructor(x, y, dx, image = 'img/enemy.png', width = 100, height = 100, dy = 1, health = 3, bulletRadius = 5, bulletSpeed = -5, periodOfShooting = 45, timeAtNow = 45) {
         this.x = x
         this.y = y
+        this.image = image
         this.width = width
         this.height = height
-        this.color = color
         this.dx = dx
         this.dy = dy
         this.isBegin = false
@@ -92,93 +69,214 @@ class Enemy {
         this.periodOfShooting = periodOfShooting
         this.timeAtNow = timeAtNow
     }
+}
 
-    Draw() {
-        ctx.beginPath()
-        ctx.rect(this.x, this.y, this.width, this.height)
-        ctx.fillStyle = this.color
-        ctx.fill()
-        ctx.closePath()
+
+function keyDownHandler(e) {
+    if(e.key === "Right" || e.key === "ArrowRight") {
+        rightPressed = true
+    } else if(e.key === "Left" || e.key === "ArrowLeft") {
+        leftPressed = true
+    } else if(e.key === "Up" || e.key === "ArrowUp") {
+        upPressed = true
+    } else if(e.key === "Down" || e.key === "ArrowDown") {
+        downPressed = true
     }
-
-    DrawCheck() {
-        if (this.health <= 0)
-        {
-            delete this
-        }
-
-        if (this.x + this.width >= canvas.width) {
-            this.dx = -this.dx
-        } else if (this.x < 0) {
-            this.dx = -this.dx
-        }
-
-        if (this.y + this.width >= canvas.height / (3 / 2)) {
-            this.dy = -this.dy
-        } else if (this.y <= 0 && this.isBegin) {
-            this.dy = -this.dy
-        }
-
-        if (this.y > 0) {
-            this.isBegin = true
-        }
+    if(e.key === 'Space') {
+        spacePressed = true
     }
-
-    Shoot () {
-        if (this.timeAtNow >= this.periodOfShooting && this.isBegin) {
-            this.timeAtNow = 0
-            enemyBullets.push(new Bullet(
-                this.x + this.width / 2,
-                this.y + this.height + this.bulletRadius,
-                this.bulletRadius,
-                this.bulletSpeed,
-                this.color
-            ))
-        }
+}
+function keyUpHandler(e) {
+    if(e.key === "Right" || e.key === "ArrowRight") {
+        rightPressed = false
+    } else if(e.key === "Left" || e.key === "ArrowLeft") {
+        leftPressed = false
+    } else if(e.key === "Up" || e.key === "ArrowUp") {
+        upPressed = false
+    } else if(e.key === "Down" || e.key === "ArrowDown") {
+        downPressed = false
     }
-
-    Update () {
-        this.timeAtNow++
-        this.Shoot()
-        this.DrawCheck()
-        this.Draw()
-        this.x += this.dx
-        this.y += this.dy
+    if(e.key === 'Space') {
+        spacePressed = false
     }
 }
 
-const friendBullets = []
-const enemyBullets = []
+addEventListener("keydown", keyDownHandler);
+addEventListener("keyup", keyUpHandler);
 
-function Animate() {
-    friendBullets.forEach(bullet => {
-        bullet.Update()
-        if (bullet.y < 0) {
-            bullet = null
-            friendBullets.shift()
-        }
-    })
-
-    enemyBullets.forEach(function (bullet, index, object) {
-        bullet.Update()
-        if (bullet.y + bullet.ballRadius > canvas.height) {
-            bullet = null
-            object.splice(index, 1)
-        }
-    })
-}
-
-addEventListener('click', () => {
-    friendBullets.push(new Bullet(
-        player.x + 25,
-        player.y - 5,
-        5,
-        8,
-        'lime',
+function CreateBoss() {
+    enemies.push(new Enemy(
+        canvas.width / 2,
+        -200,
+        4,
+        'img/boss.png',
+        300,
+        200,
+        1,
+        60,
+        10,
+        -10,
+        25,
+        25
     ))
-})
 
-const enemies = []
+}
+
+
+function FirstWave() {
+    CreateEnemies()
+}
+function SecondWave() {
+    CreateEnemies()
+    setTimeout(CreateEnemies,7500)
+
+}
+function ThirdWave() {
+    CreateBoss()
+}
+
+function keyboardCheck() {
+    if (isKeyboard) {
+        addEventListener("keydown", keyDownHandler)
+        addEventListener("keyup", keyUpHandler)
+    } else if (isMouse) {
+        addEventListener("keydown", keyDownHandler)
+        addEventListener("keyup", keyUpHandler)
+        addEventListener('click', createFriendBullet)
+    }
+}
+
+addEventListener('click', createFriendBullet)
+
+function mainDraw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    if (isMenu) {
+        drawStartMenu()
+    } else if (isGame) {
+        if (!isFirstWave) {
+            FirstWave()
+            isFirstWave = true
+        } else if (!isSecondWave && CheckEnemies()) {
+            SecondWave()
+            isSecondWave = true
+        } else if (!isThirdWave && CheckEnemies()) {
+            ThirdWave()
+            isThirdWave = true
+        } else if (CheckEnemies()) {
+            isFirstWave = false
+            isSecondWave = false
+            isThirdWave = false
+        }
+        UpdatePlayer()
+        AnimateBullets()
+        EnemiesMove()
+    } else if (isEnd) {
+        drawLoseMenu()
+    }
+
+}
+
+function drawStartMenu() {
+    ctx.beginPath()
+    ctx.font = "50px serif";
+    ctx.fillStyle = "white";
+    ctx.fillText("SCORE RECORD: " + scoreRecord, canvas.width / 2, canvas.height / 2 - 65);
+    ctx.font = "40px serif";
+    ctx.fillText("SELECT CONTROL", canvas.width / 2, canvas.height / 2);
+    ctx.fillText("Play with keyboard!(*space* for shooting)", canvas.width / 2, canvas.height / 2 + 65);
+    ctx.fillText("OR", canvas.width / 2, canvas.height / 2 + 130);
+    ctx.fillText("Play with mouse!", canvas.width / 2, canvas.height / 2 + 195);
+    ctx.closePath()
+}
+
+function drawLoseMenu() {
+    ctx.font = "50px serif";
+    ctx.fillStyle = "white";
+    ctx.fillText("YOU LOSE!", canvas.width / 2, canvas.height / 2 - 195);
+    ctx.fillText("YOUR SCORE: " + score, canvas.width / 2, canvas.height / 2 - 130);
+    ctx.fillText("SCORE RECORD: " + scoreRecord, canvas.width / 2, canvas.height / 2 - 65);
+    ctx.font = "40px serif";
+    ctx.fillText("SELECT CONTROL", canvas.width / 2, canvas.height / 2);
+    ctx.fillText("Play with keyboard!(*space* for shooting)", canvas.width / 2, canvas.height / 2 + 65);
+    ctx.fillText("OR", canvas.width / 2, canvas.height / 2 + 130);
+    ctx.fillText("Play with mouse!", canvas.width / 2, canvas.height / 2 + 195);
+}
+
+function DrawEnemy(enemy) {
+    const enemyModel = new Image();
+    enemyModel.src = enemy.image;
+    ctx.drawImage(enemyModel, enemy.x, enemy.y, enemy.width, enemy.height);
+}
+
+function EnemiesMove() {
+    enemies.forEach((enemy, enemyIndex) => {
+        friendBullets.forEach((bullet, bulletIndex) => {
+            if (PointInTexture(bullet.x + bullet.ballRadius, bullet.y, enemy) ||
+                PointInTexture(bullet.x - bullet.ballRadius, bullet.y, enemy) ||
+                PointInTexture(bullet.x, bullet.y + bullet.ballRadius, enemy) ||
+                PointInTexture(bullet.x, bullet.y - bullet.ballRadius, enemy) ||
+                PointInTexture(bullet.x + bullet.ballRadius, bullet.y + bullet.ballRadius, enemy) ||
+                PointInTexture(bullet.x - bullet.ballRadius, bullet.y + bullet.ballRadius, enemy) ||
+                PointInTexture(bullet.x + bullet.ballRadius, bullet.y - bullet.ballRadius, enemy) ||
+                PointInTexture(bullet.x - bullet.ballRadius, bullet.y - bullet.ballRadius, enemy)) {
+                bullet = null
+                friendBullets.splice(bulletIndex, 1)
+                enemy.health--
+            }
+        })
+
+        if (checkHealth(enemy)) {
+            enemy = null
+            enemies.splice(enemyIndex, 1)
+        } else {
+            EnemyUpdate(enemy)
+        }
+    })
+}
+
+function UpdateEnemyCheckForWalls(enemy) {
+    if (enemy.x + enemy.width >= canvas.width) {
+        enemy.dx = -enemy.dx
+    } else if (enemy.x < 0) {
+        enemy.dx = -enemy.dx
+    }
+
+    if (enemy.y + enemy.width >= canvas.height / (3 / 2)) {
+        enemy.dy = -enemy.dy
+    } else if (enemy.y <= 0 && enemy.isBegin) {
+        enemy.dy = -enemy.dy
+    }
+
+    if (enemy.y > 0) {
+        enemy.isBegin = true
+    }
+}
+
+function EnemyShoot (enemy) {
+    if (enemy.timeAtNow >= enemy.periodOfShooting && enemy.isBegin) {
+        enemy.timeAtNow = 0
+        enemyBullets.push(new Bullet(
+            enemy.x + enemy.width / 2,
+            enemy.y + enemy.height + enemy.bulletRadius,
+            enemy.bulletRadius,
+            enemy.bulletSpeed,
+            'red'
+        ))
+    }
+}
+
+function EnemyUpdate (enemy) {
+    enemy.timeAtNow++
+    EnemyShoot(enemy)
+    UpdateEnemyCheckForWalls(enemy)
+    DrawEnemy(enemy)
+    enemy.x += enemy.dx
+    enemy.y += enemy.dy
+}
+
+function CheckEnemies() {
+    return enemies.length <= 0
+}
 
 function CreateEnemies() {
     enemies.push(new Enemy(
@@ -187,7 +285,7 @@ function CreateEnemies() {
         4
     ))
     enemies.push(new Enemy(
-        canvas.width - 51,
+        canvas.width - 101,
         -50,
         -4
     ))
@@ -203,85 +301,154 @@ function CreateEnemies() {
     ))
 }
 
-function CreateBoss() {
-    enemies.push(new Enemy(
-        canvas.width / 2,
-        -200,
-        4,
-        200,
-        200,
-        'firebrick',
-        1,
-        120,
-        10,
-        -10,
-        30,
-        30
-    ))
+function UpdatePlayerCheckForWalls() {
+    if (player.x + player.width >= canvas.width) {
+        player.x -= player.dx
+    } else if (player.x < 0) {
+        player.x += player.dx
+    }
+
+    if (player.y + 50 > canvas.height) {
+        player.y -= player.dy
+    } else if (player.y < 0) {
+        player.y += player.dy
+    }
 }
 
-function FirstWave() {
-    CreateEnemies()
+function DrawPlayer() {
+    const playerModel = new Image();
+    playerModel.src = 'img/player.png';
+    ctx.drawImage(playerModel, player.x, player.y, player.width, player.height);
 }
 
-function SecondWave() {
-    CreateEnemies()
-    setTimeout(CreateEnemies,7500)
+function MovePlayerWithKeyboard() {
+    if (rightPressed && !leftPressed) {
+        player.x += player.dx
+    } else if (!rightPressed && leftPressed) {
+        player.x -= player.dx
+    }
+
+    if (upPressed && !downPressed) {
+        player.y -= player.dy
+    } else if (!upPressed && downPressed) {
+        player.y += player.dy
+    }
 }
 
-function ThirdWave() {
-    CreateBoss()
+function MovePlayerWithMouse() {
+    
 }
 
-function EnemiesMove() {
+function PointInTexture(pointX, pointY ,character) {
+    return pointX >= character.x &&
+        pointX <= character.x + character.width &&
+        pointY >= character.y &&
+        pointY <= character.y + character.height;
+}
+
+function UpdatePlayerCheckerForObstacle () {
+    enemyBullets.forEach((bullet, index) => {
+        if (PointInTexture(bullet.x + bullet.ballRadius, bullet.y, player) ||
+            PointInTexture(bullet.x - bullet.ballRadius, bullet.y, player) ||
+            PointInTexture(bullet.x, bullet.y + bullet.ballRadius, player) ||
+            PointInTexture(bullet.x, bullet.y - bullet.ballRadius, player) ||
+            PointInTexture(bullet.x + bullet.ballRadius, bullet.y + bullet.ballRadius, player) ||
+            PointInTexture(bullet.x - bullet.ballRadius, bullet.y + bullet.ballRadius, player) ||
+            PointInTexture(bullet.x + bullet.ballRadius, bullet.y - bullet.ballRadius, player) ||
+            PointInTexture(bullet.x - bullet.ballRadius, bullet.y - bullet.ballRadius, player)
+        ) {
+            bullet = null
+            enemyBullets.splice(index, 1)
+            if (!player.isGodMod) {
+                player.health--
+            }
+            if (player.health > 0 && !player.isGodMod) {
+                player.GodModTime = 0
+            }
+        }
+    })
+
     enemies.forEach(enemy => {
-        enemy.Update()
+        if (PointInTexture(player.x, player.y, enemy) ||
+            PointInTexture(player.x + player.width, player.y, enemy) ||
+            PointInTexture(player.x + player.width, player.y + player.height, enemy) ||
+            PointInTexture(player.x, player.y + player.height, enemy)
+        ) {
+            if (!player.isGodMod) {
+                player.health--
+            }
+            if (player.health > 0 && !player.isGodMod) {
+                player.GodModTime = 0
+            }
+        }
     })
 }
 
-ThirdWave()
-
-function mainDraw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    Animate()
-    EnemiesMove()
-    player.Move()
-    player.DrawCheck()
-    player.Draw()
-    console.log(player.x, canvas.width, player.dx)
-}
-
-const gamePlay = setInterval(mainDraw, 16)
-
-rightPressed = false
-leftPressed = false
-upPressed = false
-downPressed = false
-
-function keyDownHandler(e) {
-    if(e.key === "Right" || e.key === "ArrowRight") {
-        rightPressed = true
-    } else if(e.key === "Left" || e.key === "ArrowLeft") {
-        leftPressed = true
-    } else if(e.key === "Up" || e.key === "ArrowUp") {
-        upPressed = true
-    } else if(e.key === "Down" || e.key === "ArrowDown") {
-        downPressed = true
+function CheckPlayerGodMod () {
+    if (player.GodModTime < player.periodOfGodTime) {
+        player.GodModTime++
+        player.isGodMod = true
+    } else {
+        player.isGodMod = false
     }
-
 }
-function keyUpHandler(e) {
-    if(e.key === "Right" || e.key === "ArrowRight") {
-        rightPressed = false
-    } else if(e.key === "Left" || e.key === "ArrowLeft") {
-        leftPressed = false
-    } else if(e.key === "Up" || e.key === "ArrowUp") {
-        upPressed = false
-    } else if(e.key === "Down" || e.key === "ArrowDown") {
-        downPressed = false
+
+function checkHealth (character) {
+    return character.health <= 0
+}
+
+function UpdatePlayer() {
+    if (checkHealth(player)) {
+        isGame = false
+        isEnd = true
+    } else {
+        CheckPlayerGodMod()
+        UpdatePlayerCheckerForObstacle()
+        MovePlayerWithKeyboard()
+        UpdatePlayerCheckForWalls()
+        DrawPlayer()
     }
+}
+
+function DrawBullet(bullet) {
+    ctx.beginPath()
+    ctx.arc(bullet.x, bullet.y, bullet.ballRadius, 0, Math.PI * 2, false)
+    ctx.fillStyle = bullet.color
+    ctx.fill()
+    ctx.closePath()
+}
+
+function UpdateBullet(bullet) {
+    DrawBullet(bullet)
+    bullet.y -= bullet.velocity
+}
+
+function createFriendBullet() {
+    friendBullets.push(new Bullet(
+        player.x + 50,
+        player.y + 5,
+        5,
+        8,
+        'lime'
+    ))
+}
+
+function AnimateBullets() {
+    friendBullets.forEach((bullet, index, object) => {
+        UpdateBullet(bullet)
+        if (bullet.y < 0) {
+            bullet = null
+            object.splice(index, 1)
+        }
+    })
+
+    enemyBullets.forEach(function (bullet, index, object) {
+        UpdateBullet(bullet)
+        if (bullet.y - bullet.ballRadius >= canvas.height) {
+            bullet = null
+            object.splice(index, 1)
+        }
+    })
 
 }
-addEventListener("keydown", keyDownHandler, false);
-
-addEventListener("keyup", keyUpHandler, false);
+setInterval(mainDraw, 16)
